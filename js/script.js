97ba1ece2b6413efc83fba96a3d640f0eb38485e
LiaLobo <lia.lobo@gmail.com>
2019-10-22 15:29:56 -0300

    feito cards da section portfólio com bootstrap e efeito na descrição

---
+++ js/script.js
@@ -0,0 +1,70 @@
+$(function(){
+    
+
+    // $('.hide').mouseover(function(){
+    //     $(this).show();
+    //     }
+    // );
+
+    // $('.hide').mouseout(function(){
+    //         $(this).hide();
+    //     }
+    // );
+
+
+    // function configurateHover(){
+    //     $('.hide').off('mouseenter').on('mouseenter',
+    //         function() {
+    //             $(this).css('display', 'block')
+    //             $(this).fadeIn(1000);
+    //         }
+    //     );
+
+
+    //     $('.hide').off('mouseleave').on('mouseleave',
+    //         function() {
+    //             $(this).css('display', 'none')
+    //             $(this).fadeOut(1000);
+    //         }
+    //     );
+    
+    // }
+
+
+    // configurateHover()
+
+
+
+
+    // $('.hide').css('display', 'none')
+    // $('.hide').hover(
+
+    //     function(){
+    //         $(this).css('display', 'block')
+    //         $(this).show(1000);
+    //     },
+    //     function(){
+    //         $(this).css('display', 'none')
+    //         $(this).hide(1000);
+    //     }
+    // );
+    
+//     $('.card').mouseleave(function(){
+//     $(this).css('display', 'none')
+//     $(this).slideUp(1000);
+// }
+// );
+
+// $('.card').mouseenter(function(){
+//     $(this).css('display', 'block')
+//     $(this).slideDown(1000);
+// }
+// // );
+
+// $('.card').mouseover(function() {
+//     console.log()
+//     $(this).children('.card-body').toggleClass('esconde')
+// })
+
+
+});
